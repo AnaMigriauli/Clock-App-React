6a4d7223d2dc29af7273f8d8280e8142eee29076
AnaMigriauli <anamigriauli1994@gmail.com>
2023-07-14 13:42:47 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
 import MainContainer from "./components/MainContainer";
-import backroundImg from "./assets/mobile/dayTime.svg";
 
 function App() {
-  const containerStyles = {
-    backgroundImage: `url(${backroundImg})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    width: "375px",
-    height: "667px",
-  };
   return (
-    <div style={containerStyles}>
+    <div>
       <MainContainer />
     </div>
   );
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,11 @@
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { DateTime } from "luxon";
 import { getAllTimezones } from "countries-and-timezones";
 import sun from "../assets/mobile/Combined Shape 1.svg";
 import arrowDown from "../assets/mobile/Group 3.svg";
-
+import switchOnOff from "../assets/mobile/Combined Shape.svg";
+import classes from "./MainContainer.module.css";
+import backroundImg from "../assets/mobile/dayTime.svg";
 const MainContainer = () => {
   const [currentTime, setCurrentTime] = useState(DateTime.local());
 
@@ -17,6 +19,7 @@ const MainContainer = () => {
     };
   }, []);
   const formattedTime = currentTime.toFormat("HH:mm");
+  console.log(formattedTime);
 
   // const DayOfTheYear = currentTime.ordinal;
   // const formattedDayOfWeek = currentTime.weekday;
@@ -29,34 +32,48 @@ const MainContainer = () => {
   const countryCode = countryData ? countryData.countries[0] : "";
 
   const currentCity = currentTimeZone.split("/").slice(-1);
+  const containerStyles = {
+    backgroundImage: `url(${backroundImg})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "375px",
+    height: "667px",
+    position: "relative",
+  };
   return (
-    <Fragment>
-      <div>
-        <p>
-          “The science of operations, as derived from mathematics more
-          especially, is a science of itself, and has its own abstract truth and
-          value.”
-        </p>
-        <span>Ada Lovelace</span>
-      </div>
-      <div>
-        <div>
-          <img src={sun} alt="sun" />
-          <p>GOOD MORNING</p>
+    <div style={containerStyles} className={classes.card}>
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <div className={classes["title-container"]}>
+            <p>
+              “The science of operations, as derived from mathematics more
+              especially, is a science of itself, and has its own abstract truth
+              and value.”
+            </p>
+            <span>Ada Lovelace</span>
+          </div>
+          <img className={classes.img} src={switchOnOff} alt="" />
         </div>
-        <div>
-          <div>{formattedTime}</div>
-          <span></span>
+        <div className={classes["data-container"]}>
+          <div className={classes.dayNight}>
+            <img src={sun} alt="sun" />
+            <p>GOOD MORNING</p>
+          </div>
+          <div>
+            <div className={classes.time}>{formattedTime}</div>
+            <span></span>
+          </div>
+          <span>
+            IN {currentCity}, {countryCode}
+          </span>
         </div>
-        <p>
-          IN {currentCity}, {countryCode}
-        </p>
-        <div>
+        <div className={classes["switch-btn"]}>
           <span>MORE</span>
           <img src={arrowDown} alt="arrow" />
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 export default MainContainer;
